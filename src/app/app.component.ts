bdc09012130c633086be7a73e20f4e83dd9b79af
gourish naik <gourish@campionsoftware.com>
2022-07-18 22:57:01 +0530

    commited

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
-
+import { Component ,OnInit} from '@angular/core';
+import { FormControl, FormGroup,FormBuilder,Validators } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  myForm!: FormGroup;
+
   title = 'login';
+  url1="./assets/images/green.jpg";
+
+  constructor(private formbuilder:FormBuilder) { }
+
+  ngOnInit(): void {
+    
+    this.myForm=this.formbuilder.group({
+      email    :['',Validators.required],
+      password :['',Validators.required],
+
+
+    })
+  }
+
+
+  onSubmit(form: FormGroup) {
+    console.log('email', form.value.password);
+    console.log('password', form.value.email);
+    this.myForm.reset();
+    alert("Response has been submited")
+  }
+
+
 }
